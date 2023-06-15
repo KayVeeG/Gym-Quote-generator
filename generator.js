8d804b60c041c9fcd4ec2a5860747cf03059a2ef
Karl <karl.vongagern@gmail.com>
2023-06-15 18:41:51 +0200

    add message arrays

--- generator.js
+++ generator.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+const messagePieces = [["You are", "You are not"], ["a", "not a"], ["gym", "workout", "fitness", "calisthenics"], ["rat", "enthusiast", "workaholic", "bodybuilder", "tryharder", "beast", "girl", "lifter"]];
+const message = [];
+
+// extra: add "dedicated" with 50% chance as second last word
+
