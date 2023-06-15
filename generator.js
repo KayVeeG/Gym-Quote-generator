1db7eb2838116920d5d782f26c338c7ce9d1e1bd
Karl <karl.vongagern@gmail.com>
2023-06-15 19:00:33 +0200

    add improved code as comment at end

--- generator.js
+++ generator.js
@@ -5,13 +5,35 @@ const gymWords = [["You are", "You are not"], ["a", "not a"], ["gym", "workout",
 const gymQuote = quoteList => {
     const message = [];
 
-    for(const wordArray of quoteList){
+    for(const wordArray of quoteList){ //assemble random words
         message.push(wordArray[Math.floor(Math.random() * (wordArray.length - 1))])
     }
-    if(Math.floor(Math.random() * 2) === 1){
+    if(Math.floor(Math.random() * 2) === 1){ //50% dedicated
         message[message.length - 3] += " dedicated";
     }
+    message.push('!'); // add extra !
     return message.join(' ');
 }
 
 console.log(gymQuote(gymWords));
+
+/* -> improve code by ChatGPT
+
+const gymWords = [["You are", "You are not"], ["a", "not a"], ["gym", "workout", "fitness", "calisthenics"], ["rat", "enthusiast", "workaholic", "bodybuilder", "tryharder", "beast", "girl", "lifter"]];
+
+const getRandomElement = array => array[Math.floor(Math.random() * array.length)];
+
+const gymQuote = () => {
+    const message = gymWords.map(getRandomElement);
+    
+    if(Math.random() < 0.5){
+        message.splice(message.length - 1, 0, "dedicated");
+    }
+
+    return [...message, '!'].join(' ');
+}
+
+const logQuote = () => console.log(gymQuote());
+
+logQuote();
+*/
