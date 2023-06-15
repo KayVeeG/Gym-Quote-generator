6dcb571a3f4f0dc884499751738f3a1eee496a66
Karl <karl.vongagern@gmail.com>
2023-06-15 18:52:23 +0200

    add gymQuote Function

--- generator.js
+++ generator.js
@@ -1,5 +1,14 @@
-const messagePieces = [["You are", "You are not"], ["a", "not a"], ["gym", "workout", "fitness", "calisthenics"], ["rat", "enthusiast", "workaholic", "bodybuilder", "tryharder", "beast", "girl", "lifter"]];
-const message = [];
+const gymWords = [["You are", "You are not"], ["a", "not a"], ["gym", "workout", "fitness", "calisthenics"], ["rat", "enthusiast", "workaholic", "bodybuilder", "tryharder", "beast", "girl", "lifter"]];
 
 // extra: add "dedicated" with 50% chance as second last word
 
+const gymQuote = quoteList => {
+    const message = [];
+
+    for(const wordArray of quoteList){
+        message.push(wordArray[Math.floor(Math.random() * (wordArray.length - 1))])
+    }
+    if(Math.floor(Math.random() * 2) === 1){
+        message[message.length - 2] += "dedicated";
+    }
+}
